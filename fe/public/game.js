7e78c499f689b4aa8c5d7eb25d06d521d0d5678a
Christiancaicedo <christian.caicedo96@gmail.com>
2023-04-21 20:46:58 -0500

    updated base code and started game loop

--- fe/public/game.js
+++ fe/public/game.js
@@ -27,64 +27,103 @@ const boardNumbers = {
     six: {
         num: 6,
         up: true,
-        down:false
+        down: false
     },
     seven: {
         num: 7,
         up: true,
-        down:false
+        down: false
     },
     eight: {
         num: 8,
         up: true,
-        down:false
+        down: false
     },
     nine: {
         num: 9,
         up: true,
-        down:false
+        down: false
     },
     ten: {
         num: 10,
         up: true,
-        down:false
+        down: false
     },
     eleven: {
         num: 11,
         up: true,
-        down:false
+        down: false
     },
     twelve: {
         num: 12,
         up: true,
-        down:false
+        down: false
     },
 }
 
+const roll1 = Math.floor(Math.random() * 6 + 1);
+const roll2 = Math.floor(Math.random() * 6 + 1);
+let diceTotal = roll1 + roll2;
 
-document.getElementById('rollDice').addEventListener('click', () => {
-    const roll1 = Math.floor(Math.random()* 6 + 1);
-    const roll2 = Math.floor(Math.random()* 6 + 1);
-    const diceTotal = roll1 + roll2;
+const diceButton = document.getElementById('rollDice')
+const resetButton = document.getElementById('reset')
+const dRoll1 = document.getElementById('dRoll1')
+const dRoll2 = document.getElementById('dRoll2')
+const rollTotal = document.getElementById('dTotal')
+let diceReset = false
 
-    document.getElementById('dRoll1').innerHTML = `Dice Roll 1: ${roll1}`;
-    document.getElementById('dRoll2').innerHTML = `Dice Roll 2: ${roll2}`;
-    document.getElementById('dTotal').innerHTML = `Dice Total: ${diceTotal}`;
-    if (diceTotal !== 0) {
-        document.getElementById('rollDice').disabled = true;
+// need to have a game loop that resets with button press
 
-    } else {
-        document.getElementById('rollDice').disabled = false;
+diceButton.addEventListener('click', () => {
+    if (!diceReset) {
+        dRoll1.innerHTML = `Dice Roll 1: ${roll1}`;
+        dRoll2.innerHTML = `Dice Roll 2: ${roll2}`;
+        rollTotal.innerHTML = `Dice Total: ${diceTotal}`;
+        if (diceTotal !== 0) {
+            diceButton.disabled = true;
+        }
     }
 })
 
-// get element id when element clicks i.e. '1', '2' ,'3'
-// once id is found, reference in board object to change up to false, down to true
-// logic check that disables clicking the id that up is false
-const id = document.getElementsByTagName('p');
-for (let i = 0; i < id.length; i++) {
-    console.log(i.id)
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    diceReset = false
+    diceButton.disabled = false;
+    dRoll1.innerHTML = `Dice Roll 1: `;
+    dRoll2.innerHTML = `Dice Roll 2: `;
+    rollTotal.innerHTML = `Dice Total: `;
 }
+
+
+
+
+
+// Grab number id from html and check it again total & boardNumbers object
+// if total - id >= 0, disable or enable number for game
+
+function numClick(numClicked) {
+    let storeId = numClicked
+    console.log(storeId)
+    if (storeId - diceTotal <= 0) {
+        console.log(storeId)
+        for (number in boardNumbers) {
+            console.log(number)
+            if (storeId = number.num) {
+                console.log(number)
+            }
+        }
+    }
+}
+
+// boardNumbers.numClicked.up = false;
+//                 console.log(boardNumbers.numClicked.up)
+//                 boardNumbers.numClicked.down = true;
+//                 console.log(boardNumbers.numClicked.down)
+//                 boardNumbers.numClicked.disabled = true;
+//                 let updateTotal = diceTotal - numClicked;
+//                 rollTotal.innerHTML = `Dice Total: ${updateTotal}`
+//                 console.log(numClicked)
 
 // function boardDown(){
 //     for(let i = 0; i < boardNumbers.length(); i++) {
